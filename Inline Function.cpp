a8bb72b9f47c87a16600425bae4415616d338635
Hayeon Oh <59410853+HAMA-DL-dev@users.noreply.github.com>
2020-11-20 14:19:21 +0900

    인라인 함수란?

---
+++ Inline Function.cpp
@@ -0,0 +1,31 @@
+// Non-inline function
+#include <iostream>
+using namespace std;
+
+int add2(int n1, int n2) {
+	int out = 0;
+	out = n1 + n2;
+	return out;
+}
+
+int main() {
+	int result = 0;
+	result = add2(3, 5);
+	cout << "The result is " << result << endl;
+}
+
+// Inline function
+#include <iostream>
+using namespace std;
+
+inline int add2(int n1,int n2) {
+	int out = 0;
+	out = n1 + n2;
+	return out;
+}
+
+int main() {
+	int result = 0;
+	result = add2(3, 5);
+	cout << "The result is " << result << endl;
+}
